89ca0371bea5385426fe431ac7e38d927eb4412c
chikebaba <114248659+chikebaba@users.noreply.github.com>
2023-01-09 19:15:21 +0100

    Create GenderState.kt

---
+++ app/src/main/java/com/example/kotlin_course_project/repository/GenderState.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin_course_project.repository
+
+enum class GenderState(val title: String) {
+    FEMALE("female"),
+    MALE("male"),
+    GENDERLESS("genderless"),
+    UNKNOWN("unknown"),
+    NONE("")
+}
